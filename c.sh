3da46acab8208cf86d6aa88a5952b5c5f0905c97
Peter Szabo <pts@fazekas.hu>
2013-11-03 12:45:25 +0100

    added compilation script and config.h

---
+++ c.sh
@@ -0,0 +1,15 @@
+#! /bin/bash --
+BINPREFIX='/home/pts/prg/pts-mini-gpl/uevalrun/cross-compiler/bin/i686-'
+CC="${BINPREFIX}gcc -static -fno-stack-protector"
+AR="${BINPREFIX}ar"
+RANLIB="${BINPREFIX}ranlib"
+
+set -ex
+rm -f *.o *.a
+$CC -s -Os -W -Wall \
+    -Wno-unused-parameter \
+    -DHAVE_CONFIG_H -DMISSING_GLOB_PATTERN_P -D_GNU_SOURCE -c \
+    popt.c poptparse.c poptconfig.c popthelp.c poptint.c
+$AR cr libpopt.a *.o
+$RANLIB libpopt.a
+: c.sh OK.
